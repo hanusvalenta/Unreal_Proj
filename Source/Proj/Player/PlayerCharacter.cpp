addefe395d5d4b91cc956375bd93c76a7f0b69a9
Kemny <l.krchov@gmail.com>
2022-08-06 18:53:17 +0200

    First person character

--- Source/Proj/Player/PlayerCharacter.cpp
+++ Source/Proj/Player/PlayerCharacter.cpp
@@ -3,12 +3,18 @@
 
 #include "Player/PlayerCharacter.h"
 
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/PawnMovementComponent.h"
+
 // Sets default values
 APlayerCharacter::APlayerCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(GetCapsuleComponent());
 }
 
 // Called when the game starts or when spawned
@@ -30,12 +36,35 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	UInputComponent* I = PlayerInputComponent;
-	I->BindKey(EKeys::SpaceBar, EInputEvent::IE_Pressed, this, &APlayerCharacter::MyJump);
+	// NOTE: Not update, better performance
+	PlayerInputComponent->BindKey(EKeys::SpaceBar, EInputEvent::IE_Pressed, this, &APlayerCharacter::Jump);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::MoveForward);
+	
+	PlayerInputComponent->BindAxis("MouseRight", this, &APlayerCharacter::RotateCameraAroundCharacter);
+	PlayerInputComponent->BindAxis("MouseUp", this, &APlayerCharacter::RotateCameraUpDown);
 }
 
-void APlayerCharacter::MyJump()
+void APlayerCharacter::MoveRight(float Axis)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Jumped"));
+	GetMovementComponent()->AddInputVector(Camera->GetComponentRotation().RotateVector(FVector::RightVector * Axis * GetWorld()->GetDeltaSeconds() * MovementSpeed));
+}
+
+void APlayerCharacter::MoveForward(float Axis)
+{
+	GetMovementComponent()->AddInputVector(Camera->GetComponentRotation().RotateVector(FVector::ForwardVector * Axis * GetWorld()->GetDeltaSeconds() * MovementSpeed));
+}
+
+void APlayerCharacter::RotateCameraUpDown(float Axis)
+{
+	FRotator rot = Camera->GetComponentRotation() + FRotator(Axis * GetWorld()->GetDeltaSeconds() * CameraSensitivity.Y, 0, 0);
+	rot.Pitch = FMath::Clamp(rot.Pitch, PitchLimits.X, PitchLimits.Y);
+	
+	Camera->SetWorldRotation(rot);
+}
+
+void APlayerCharacter::RotateCameraAroundCharacter(float Axis)
+{
+	Camera->SetWorldRotation(Camera->GetComponentRotation() + FRotator(0, Axis * GetWorld()->GetDeltaSeconds() * CameraSensitivity.X, 0));
 }
 
--- Source/Proj/Player/PlayerCharacter.h
+++ Source/Proj/Player/PlayerCharacter.h
@@ -6,26 +6,58 @@
 #include "GameFramework/Character.h"
 #include "PlayerCharacter.generated.h"
 
+class UCameraComponent;
+
 UCLASS()
 class PROJ_API APlayerCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
+	
 	// Sets default values for this character's properties
 	APlayerCharacter();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
 private:
-	void MyJump();
+
+	// Moves either to the left or right, depending on the axis
+	void MoveRight(float Axis);
+	
+	// Moves either to the forward or backward, depending on the axis
+	void MoveForward(float Axis);
+
+	// Rotates camera along the Yaw axis depending on the passed axis
+	void RotateCameraUpDown(float Axis);
+	
+	// Rotates camera along the Pitch axis depending on the passed axis
+	void RotateCameraAroundCharacter(float Axis);
+
+protected: // Movement variables
+	
+	UPROPERTY(EditDefaultsOnly, Category="Movement")
+	float MovementSpeed = 100;
+
+protected: // Camera variables
+
+	UPROPERTY(EditDefaultsOnly, Category="Camera")
+	FVector2D PitchLimits = FVector2D(-40, 40);
+	
+	UPROPERTY(EditDefaultsOnly, Category="Camera")
+	FVector2D CameraSensitivity = FVector2D(100,50);
+	
+protected: // Components
+
+	UPROPERTY(EditDefaultsOnly)
+	UCameraComponent* Camera;
 };
